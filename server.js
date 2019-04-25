7305973a4f6d998fbead8dd407af8fa5a5d9d902
Lei Xu <leixu216@gmail.com>
2019-04-25 16:41:49 +0800

    update port to use env var

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express'),
-  app = express(),
-  port = 3000;
+  app = express();
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 app.use(express.static('public'));
 
@@ -14,3 +20,72 @@ if (! module.parent) {
 module.exports = app
 
 console.log("Server running on port " + port);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+//server.listen(port);
+//server.on('error', onError);
+//server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+yuy
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
