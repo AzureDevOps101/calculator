bf84cf6c0def79d08ca94e8b8aeee4495d61c9fb
Lei Xu <leixu@lean-soft.cn>
2019-04-25 21:46:17 +0800

    break it again

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
   var operations = {
     // +a will convert a into integer type to make sure the operation is correct
-    'add':      function(a,b) { return +a + +b },
+    'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
