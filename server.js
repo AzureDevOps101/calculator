dcd17443e54a1699cb461d2fe7d8f2c03aa22664
Lei Xu <leixu@leansoftx.com>
2021-11-02 09:49:42 +0800

    smoke test 866

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ console.log("Smoke Test Windows 860");
 console.log("Smoke Test Windows 860 - VM");
 console.log("Smoke test 866 VM");
 console.log("Smoke Test Windows 860 - VM + 1");
+console.log("Smoke test Windows 866 VM");
 
 /**
  * Normalize a port into a number, string, or false.
